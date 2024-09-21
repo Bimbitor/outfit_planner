da555a711e19a42551b5a0c6dd8def88c0599d5d
Felipe <ipelk18@gmail.com>
2024-09-21 17:13:52 -0500

    Cambio de login

--- app/src/main/java/com/example/outfit_planner/MainActivity.java
+++ app/src/main/java/com/example/outfit_planner/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        String myString = "Hola mundo";
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
